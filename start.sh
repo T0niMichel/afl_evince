bc9d5768880900a73fb85bd7a901ab293e9cce51
Toni Michel <tm@kungf.ooo>
2016-06-07 13:12:33 +0200

    Fixed shellscript to a funktional state. Problem is still there:
    PROGRAM ABORT : All test cases time out, giving up!
    
    Increasing time with the -t flag doesn't seem to help.

--- start.sh
+++ start.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
-#FUZZ_ID=evince
-IN=/data/input
-OUT=/data/output
-MOUNT=.
-AFL_IMAGE=tonimichel/afl_evince
-
-
-FUZZ_TARGET=/usr/local/bin/evince
+export INPUT='/data/input'
+export OUTPUT='/data/output'
+export MOUNT=`pwd`
+export AFL_IMAGE='tonimichel/afl_evince'
+export FUZZ_TARGET='/usr/local/bin/evince'
 
 sudo echo core >/proc/sys/kernel/core_pattern
 cd /sys/devices/system/cpu
 sudo echo performance | tee cpu*/cpufreq/scaling_governor
 cd -
-docker run -m 2g -ti -e DISPLAY=$DISPLAY \
+pwd
+docker run -m 6g -ti --rm  \
+       -e DISPLAY=$DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix  \
-       -v .:/data \
-       --ulimit core=-1 tonimichel/afl_evince \
-       afl-fuzz -Z 3 -t 400+ms -m 2048 -i $IN  -o $OUT  $FUZZ_TARGET
+       -v $MOUNT:/data \
+       --ulimit core=-1 \
+       --ulimit data=-1 \
+       tonimichel/afl_evince \
+       afl-fuzz -Z 0-3 -t 2000+ -m 4096 -i $INPUT  -o $OUTPUT  $FUZZ_TARGET
 
