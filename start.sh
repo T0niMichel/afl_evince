65a208b304e09f96682678e4963a86a5f9ffa9e3
Toni Michel <tm@kungf.ooo>
2016-06-07 12:21:24 +0200

    Problem running evince...
    [-] PROGRAM ABORT : All test cases time out, giving up!
             Location : perform_dry_run(), afl-fuzz.c:2763
    
    Increasing time with the -t flag doesn't seem to help.

--- start.sh
+++ start.sh
@@ -9,9 +9,13 @@ AFL_IMAGE=tonimichel/afl_evince
 
 FUZZ_TARGET=/usr/local/bin/evince
 
-
-docker run -m 8g -ti -e DISPLAY=$DISPLAY \
+sudo echo core >/proc/sys/kernel/core_pattern
+cd /sys/devices/system/cpu
+sudo echo performance | tee cpu*/cpufreq/scaling_governor
+cd -
+docker run -m 2g -ti -e DISPLAY=$DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix  \
        -v .:/data \
        --ulimit core=-1 tonimichel/afl_evince \
-       afl-fuzz -Z 0-3 -t 100+ms -m 2048 -i $IN  -o $OUT  $FUZZ_TARGET
+       afl-fuzz -Z 3 -t 400+ms -m 2048 -i $IN  -o $OUT  $FUZZ_TARGET
+
